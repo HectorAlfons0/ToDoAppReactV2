a0e23712f4971682091fcb6e1a5f8bb117844d2b
Hector Elias Alfonso Bogado <5278253@elmejor.com.py>
2024-12-04 17:48:31 -0300

    render props, render function, react children, react clone element

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import './TodoCounter.css';
+import React from "react";
+import "./TodoCounter.css";
 
-function TodoCounter({ totalTodos,completedTodos, loading }) {
-
-  
+function TodoCounter({ totalTodos, completedTodos, loading }) {
   return (
-    <div className='TodoCounter'>
-    <h2 className={`TodoCounter ${!!loading && "TodoCounter--loading"}`}>
-    Has completado {completedTodos} de {totalTodos} TODOs</h2>
-    {completedTodos === totalTodos && totalTodos > 0 && (
+    <div className="TodoCounter">
+      <h2 className={`TodoCounter ${!!loading && "TodoCounter--loading"}`}>
+        Has completado {completedTodos} de {totalTodos} TODOs
+      </h2>
+      {completedTodos === totalTodos && totalTodos > 0 && (
         <h2>¡Felicidades! Has completado todas tus tareas 🎉</h2>
-    )}
-</div>
-    
-    
+      )}
+    </div>
   );
 }
 
